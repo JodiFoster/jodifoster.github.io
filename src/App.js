844cd12cfca94f70e913b3e959b44c6410fca26d
Jodi Foster <jodi@freeup.io>
2021-03-08 09:49:07 +0000

    update url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import avataar from './assets/avataar.svg'
+import github from './assets/github.svg'
+import linkedIn from './assets/linkedIn.svg'
+import email from './assets/email.svg'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="root">
+            <div className="Centered">
+                <div>
+                    <img src={avataar} className="avataar" alt="logo" />
+                </div>
+                <p className="Text-name">JODI FOSTER</p>
+                <p className="Text">SOFTWARE DEVELOPER</p>
+                <div className="Social-container">
+                    <a href="https://www.linkedin.com/in/jodi-foster-092415b1/">
+                        <img src={linkedIn} className="Social-icon" alt="logo" />
+                    </a>
+                    <a href="https://github.com/JodiFoster">
+                        <img src={github} className="Social-icon" alt="logo" />
+                    </a>
+                    <a href="mailto:jodiallysonfoster@gmail.com">
+                        <img src={email} className="Social-icon" alt="logo" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
