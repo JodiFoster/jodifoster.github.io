dabc03b384a31d42a2e299464b1836a43a64224f
Jodi Foster <jodi@freeup.io>
2021-03-08 11:32:55 +0000

    edit styling

--- src/App.js
+++ src/App.js
@@ -7,26 +7,28 @@ import './App.css'
 
 function App() {
     return (
-        <div id="root">
-            <div className="Centered">
-                <div>
-                    <img src={avataar} className="avataar" alt="logo" />
+        <>
+            <header className="body">
+                <div className="Centered">
+                    <img src={avataar} alt="logo" className="Avataar" />
+                    <div className="Text-container">
+                        <p className="Text-name">JODI FOSTER</p>
+                        <p className="Text">SOFTWARE DEVELOPER</p>
+                    </div>
+                    <div className="Social-container">
+                        <a href="https://www.linkedin.com/in/jodi-foster-092415b1/">
+                            <img src={linkedIn} className="Social-icon" alt="logo" />
+                        </a>
+                        <a href="https://github.com/JodiFoster">
+                            <img src={github} className="Social-icon" alt="logo" />
+                        </a>
+                        <a href="mailto:jodiallysonfoster@gmail.com">
+                            <img src={email} className="Social-icon" alt="logo" />
+                        </a>
+                    </div>
                 </div>
-                <p className="Text-name">JODI FOSTER</p>
-                <p className="Text">SOFTWARE DEVELOPER</p>
-                <div className="Social-container">
-                    <a href="https://www.linkedin.com/in/jodi-foster-092415b1/">
-                        <img src={linkedIn} className="Social-icon" alt="logo" />
-                    </a>
-                    <a href="https://github.com/JodiFoster">
-                        <img src={github} className="Social-icon" alt="logo" />
-                    </a>
-                    <a href="mailto:jodiallysonfoster@gmail.com">
-                        <img src={email} className="Social-icon" alt="logo" />
-                    </a>
-                </div>
-            </div>
-        </div>
+            </header>
+        </>
     )
 }
 
